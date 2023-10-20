1871c71c2ccc675fb358c77af423a13fd3788f61
alena <vorozhtsova_alena@mail.ru>
2023-10-20 18:50:12 +0500

    Задание 17.1.4

---
+++ Module17/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Module17
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public string GetDayOfWeek(int day)
+        {
+            switch (day)
+            {
+                case 1:
+                    return "Monday";
+                case 2:
+                    return "Tuesday";
+                case 3:
+                    return "Wednesday";
+                case 4:
+                    return "Thursday";
+                case 5:
+                    return "Friday";
+                case 6:
+                    return "Saturday";
+                case 7:
+                    return "Sunday";
+
+                default:
+                    throw new InvalidOperationException("День недели должен быть в диапазоне 1 to 7");
+            }
+        }
+    }
+}
